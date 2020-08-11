108f21b3e2f3b56262797cdbb0405a427477f3e2
mattkastner <matthewkastner@gmail.com>
2020-08-11 16:03:44 -0600

    added slight changes to the navbar to hold the login feature

---
+++ components/Common/Login.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="section section-javascript" id="javascriptComponents">
+    <n-button type="success" @click.native="modals.login = true">
+      <i class="now-ui-icons users_single-02"></i> Login Modal
+    </n-button>
+    <modal
+      :show.sync="modals.login"
+      modal-classes="modal-login"
+      class="modal-success"
+    >
+      <div
+        class="card card-plain card-login"
+        data-background-color=""
+        slot="base-content"
+      >
+        <div class="modal-header justify-content-center">
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-hidden="true"
+          >
+            <i class="now-ui-icons ui-1_simple-remove"></i>
+          </button>
+          <div class="header header-primary text-center">
+            <div class="logo-container">
+              <img v-lazy="'img/now-logo.png'" alt="" />
+            </div>
+          </div>
+        </div>
+        <div class="modal-body">
+          <form class="form" method="" action="">
+            <div class="card-body">
+              <fg-input
+                class="no-border input-lg"
+                addon-left-icon="now-ui-icons users_circle-08"
+                placeholder="First Name..."
+              >
+              </fg-input>
+
+              <fg-input
+                class="no-border input-lg"
+                type="password"
+                addon-left-icon="now-ui-icons ui-1_lock-circle-open"
+                placeholder="Password..."
+              >
+              </fg-input>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer text-center">
+          <a
+            href="#pablo"
+            class="btn btn-neutral btn-round btn-lg btn-block text-success"
+            >Get Started</a
+          >
+        </div>
+      </div>
+    </modal>
+  </div>
+</template>
+<script>
+import {
+  Button,
+  Modal,
+  FormGroupInput
+} from "@/components";
+
+export default {
+  components: {
+    Modal,
+    [FormGroupInput.name]: FormGroupInput,
+  },
+  data() {
+    return {
+      modals: {
+        login: false,
+      },
+    };
+  },
+};
+</script>
+<style lang="scss">
+#javascriptComponents {
+  .modal-buttons {
+    .btn + .btn {
+      margin-left: 3px;
+    }
+  }
+}
+</style>
--- components/Common/MainNavbar.vue
+++ components/Common/MainNavbar.vue
@@ -2,16 +2,15 @@
   <navbar type="success" menu-classes="ml-auto">
     <div class="navbar-translate">
       <a class="navbar-brand" href="/">Emergency Ready App</a>
-<!--      <button class="navbar-toggler" type="button" data-toggle="collapse"-->
-<!--              data-target="#example-navbar-info" aria-controls="navbarSupportedContent"-->
-<!--              aria-expanded="false" aria-label="Toggle navigation">-->
-<!--        <span class="navbar-toggler-bar bar1"></span>-->
-<!--        <span class="navbar-toggler-bar bar2"></span>-->
-<!--        <span class="navbar-toggler-bar bar3"></span>-->
-<!--      </button>-->
+      <!--      <button class="navbar-toggler" type="button" data-toggle="collapse"-->
+      <!--              data-target="#example-navbar-info" aria-controls="navbarSupportedContent"-->
+      <!--              aria-expanded="false" aria-label="Toggle navigation">-->
+      <!--        <span class="navbar-toggler-bar bar1"></span>-->
+      <!--        <span class="navbar-toggler-bar bar2"></span>-->
+      <!--        <span class="navbar-toggler-bar bar3"></span>-->
+      <!--      </button>-->
     </div>
     <template slot="navbar-menu">
-
       <li class="nav-item">
         <a class="nav-link" href="/">
           <p>Home</p>
@@ -60,50 +59,54 @@
     </template>
   </navbar>
 
-<!--    <template slot="navbar-menu">-->
-<!--      <drop-down tag="li" title="Features" icon="now-ui-icons files_paper" class="nav-item">-->
-<!--        <nav-link to="/custom-branded-emergency-ready-app">-->
-<!--          <i class="now-ui-icons ui-2_settings-90"></i> Custom Branded App-->
-<!--        </nav-link>-->
-<!--        <nav-link to="/send-emergency-notifications">-->
-<!--          <i class="now-ui-icons sport_user-run"></i> Send Emergency Notifications-->
-<!--        </nav-link>-->
-<!--        <nav-link to="/emergency-response-plans">-->
-<!--          <i class="now-ui-icons education_paper"></i> Emergency Response Plans-->
-<!--        </nav-link>-->
-<!--        <nav-link to="/contacts-info-in-emergency-ready-app">-->
-<!--          <i class="now-ui-icons business_money-coins"></i> Add Your Contacts Info-->
-<!--        </nav-link>-->
-<!--        <nav-link to="/interactive-emergency-preparedness-kits">-->
-<!--          <i class="now-ui-icons ui-2_chat-round"></i> Interactive Preparedness Kits-->
-<!--        </nav-link>-->
-<!--        <nav-link to="/hazard-guides">-->
-<!--          <i class="now-ui-icons ui-2_chat-round"></i> Hazard Guides-->
-<!--        </nav-link>-->
-<!--      </drop-down>-->
-<!--    </template>-->
-<!--  </navbar>-->
+  <!--    <template slot="navbar-menu">-->
+  <!--      <drop-down tag="li" title="Features" icon="now-ui-icons files_paper" class="nav-item">-->
+  <!--        <nav-link to="/custom-branded-emergency-ready-app">-->
+  <!--          <i class="now-ui-icons ui-2_settings-90"></i> Custom Branded App-->
+  <!--        </nav-link>-->
+  <!--        <nav-link to="/send-emergency-notifications">-->
+  <!--          <i class="now-ui-icons sport_user-run"></i> Send Emergency Notifications-->
+  <!--        </nav-link>-->
+  <!--        <nav-link to="/emergency-response-plans">-->
+  <!--          <i class="now-ui-icons education_paper"></i> Emergency Response Plans-->
+  <!--        </nav-link>-->
+  <!--        <nav-link to="/contacts-info-in-emergency-ready-app">-->
+  <!--          <i class="now-ui-icons business_money-coins"></i> Add Your Contacts Info-->
+  <!--        </nav-link>-->
+  <!--        <nav-link to="/interactive-emergency-preparedness-kits">-->
+  <!--          <i class="now-ui-icons ui-2_chat-round"></i> Interactive Preparedness Kits-->
+  <!--        </nav-link>-->
+  <!--        <nav-link to="/hazard-guides">-->
+  <!--          <i class="now-ui-icons ui-2_chat-round"></i> Hazard Guides-->
+  <!--        </nav-link>-->
+  <!--      </drop-down>-->
+  <!--    </template>-->
+  <!--  </navbar>-->
 </template>
 
 <script>
-  import { DropDown, NavbarToggleButton, Navbar, NavLink } from '@/components/UIKit';
-  import { Popover } from 'element-ui'
-  export default {
-    name: 'main-navbar',
-    props: {
-      transparent: Boolean,
-      colorOnScroll: Number,
-    },
-    components: {
-      DropDown,
-      Navbar,
-      NavbarToggleButton,
-      NavLink,
-      [Popover.name]: Popover
-    }
-  }
+import {
+  DropDown,
+  NavbarToggleButton,
+  Navbar,
+  NavLink,
+} from "@/components/UIKit";
+import { Popover } from "element-ui";
+import Login from "@/components/Login";
+export default {
+  name: "main-navbar",
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number,
+  },
+  components: {
+    DropDown,
+    Navbar,
+    NavbarToggleButton,
+    NavLink,
+    [Popover.name]: Popover,
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
