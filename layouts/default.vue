c1917bbac1aaefc11e70fc104075a856ef22ffdc
Steven Moon <stevenmoon@Stevens-MacBook-Pro.local>
2020-09-07 11:38:11 -0600

    add chatlio

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
     <contact-us></contact-us>
 <!--    <subscribe :showImage="true"></subscribe>-->
     <main-footer></main-footer>
+    <div v-html="chatlio"></div>
   </div>
 </template>
 <script>
@@ -22,7 +23,12 @@
       MainNavbar,
       ContactUs,
       Subscribe,
-    }
+    },
+    data() {
+      return {
+        chatlio: "<script type=\"text/javascript\"> window._chatlio = window._chatlio||[]; !function(){ var t=document.getElementById(\"chatlio-widget-embed\");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=[\"configure\",\"identify\",\"track\",\"show\",\"hide\",\"isShown\",\"isOnline\", \"page\", \"open\", \"showOrHide\"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement(\"script\"),c=document.getElementsByTagName(\"script\")[0];n.id=\"chatlio-widget-embed\",n.src=\"https://w.chatlio.com/w.chatlio-widget.js\",n.async=!0,n.setAttribute(\"data-embed-version\",\"2.3\"); n.setAttribute('data-widget-id','de2cd43d-9fce-4f7c-7bfd-5f73c1e6d35e');c.parentNode.insertBefore(n,c); }(); <\/script>",
+      }
+    },
   }
 </script>
 <style lang="scss">
