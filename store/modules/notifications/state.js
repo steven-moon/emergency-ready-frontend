27c1b376c95a58a65545f4e8ef30e787ea5a4ac5
Steven Moon <steve@clevercoding.com>
2024-06-26 22:47:01 -0600

    working on bug

--- store/modules/notifications/state.js
+++ store/modules/notifications/state.js
@@ -1,22 +1,26 @@
 export default {
-	/**
-	 * Notifications for Company App
-	 *
-	 * @type {Array}
-	 */
-	notifications: [],
+	state: () => ({
+		/**
+		 * Notifications for Company App
+		 *
+		 * @type {Array}
+		 */
+		notifications: [],
 
-	/**
-	 * Single Notification
-	 *
-	 * @type {Array}
-	 */
-	notification: null,
+		/**
+		 * Single Notification
+		 *
+		 * @type {Array}
+		 */
+		notification: null,
 
-	/**
-	 * Notification Types for Company App
-	 *
-	 * @type {Array}
-	 */
-	notificationTypes: [],
+		/**
+		 * Notification Types for Company App
+		 *
+		 * @type {Array}
+		 */
+		notificationTypes: [],
+	}),
+
+	// Add other properties like getters, actions, and mutations here if needed
 };
